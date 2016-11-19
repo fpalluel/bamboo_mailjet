e716d689f243d4595ffcf857c6e1172cd7cc35b7
Fred <fpalluel@gmail.com>
2016-11-19 11:29:29 +0100

    Fix mix.exs for hex publish

--- mix.exs
+++ mix.exs
@@ -2,15 +2,19 @@ defmodule BambooMailjet.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :bamboo_mailjet,
-     version: "0.0.1",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :bamboo_mailjet,
+      version: "0.0.1",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      description: "A Mailjet adapter for Bamboo",
+      package: package,
+      deps: deps()
+    ]
   end
 
-  def package do
+  defp package do
     [
       maintainers: ["moxide"],
       licenses: ["MIT"],
@@ -19,7 +23,9 @@ defmodule BambooMailjet.Mixfile do
   end
 
   def application do
-    [applications: [:logger, :bamboo]]
+    [
+      applications: [:logger, :bamboo]
+    ]
   end
 
   defp deps do
@@ -27,8 +33,8 @@ defmodule BambooMailjet.Mixfile do
       {:bamboo, "~> 0.7.0"},
       {:credo, "~> 0.5", only: [:dev, :test]},
       {:cowboy, "~> 1.0", only: [:test, :dev]},
-      {:ex_doc, "~> 0.14", only: :docs},
-      {:inch_ex, ">= 0.0.0", only: :docs}
+      {:ex_doc, "~> 0.14", only: :dev},
+      {:inch_ex, ">= 0.0.0", only: :dev}
     ]
   end
 end
